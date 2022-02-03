b986df19c03fbf9bd65c3544d70b2d41b8d7eac6
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-02-03 11:05:54 +0100

    (refactor): Add getGatewayCredentials() to env file

--- src/config/environments/env.ts
+++ src/config/environments/env.ts
@@ -1,3 +1,13 @@
 export function isProduction(): boolean {
   return process.env.NODE_ENV === 'staging';
 }
+
+export function getGatewayCredentials(): { user: string, pass: string } {
+  const { GATEWAY_USER, GATEWAY_PASS } = process.env;
+
+  if (!GATEWAY_USER || !GATEWAY_PASS) {
+    throw new Error('Gateway credentials are not set');
+  }
+
+  return { user: GATEWAY_USER, pass: GATEWAY_PASS };
+}
